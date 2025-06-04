3d6ae3367fb23909a5db50ad4b16bd8fff478503
Th3S1l3ntW0lf <65628541+Th3S1l3ntW0lf@users.noreply.github.com>
2025-06-04 08:41:12 -0500

    support@ledr.io => support@midwksl.net

--- src/avesterra/avesterra.py
+++ src/avesterra/avesterra.py
@@ -4,7 +4,7 @@ This file is part of the Orchestra library, which helps developer use our Orches
 The Orchestra library is a free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
 The Orchestra library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License along with the Orchestra library. If not, see <https://www.gnu.org/licenses/>.
-If you have any questions, feedback or issues about the Orchestra library, you can contact us at support@ledr.io.
+If you have any questions, feedback or issues about the Orchestra library, you can contact us at support@midwksl.net.
 """
 
 from __future__ import annotations
